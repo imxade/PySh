43f206dfc214d09a4e600ea7dabd1769d61cd1f9
XADE <xade@noreply.codeberg.org>
2025-06-26 02:02:49 +0200

    pip module (#1)
    Reviewed-on: https://codeberg.org/zz/PySh/pulls/1

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysh',
+    version='0.1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pysh=pysh:main',
+        ],
+    },
+    author='Rituraj Basak',
+    description='A basic Unix shell and Python REPL',
+    url="https://codeberg.org/zz/PySh", 
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
